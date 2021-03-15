b18a92ecea81079ea2f4a0b32d2b4f9b675130f8
epenkrart <e.penkrat@whiteskydigital.com>
2021-03-15 08:29:39 +0300

    init server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 (async function () {
     const app = new (require('express'))();
-    console.log('Server Running in Developer Environment 555');
-
+    console.log('Server is Run');
+    app.listen(3000, error => {
+        if (error) {
+            console.log(error)
+        } else {
+            console.info('Running on port 3000');
+        }
+    })
 }());
